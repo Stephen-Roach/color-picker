9618ab01cca1003171d7dcb36b5d30e31b5405cc
Stephen-Roach <stephenroach21@gmail.com>
2021-01-27 22:13:28 -0600

    inital commit

---
+++ src/Button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Button = (props) => {
+  return <button class='btn btn-dark'>UNLOCK</button>;
+};
+
+export default Button;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,122 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Swatch from './Swatch';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    this.state = {
+      swatch1: { color: this.randomColor(), isLocked: false },
+      swatch2: { color: this.randomColor(), isLocked: false },
+      swatch3: { color: this.randomColor(), isLocked: false },
+      swatch4: { color: this.randomColor(), isLocked: false },
+      swatch5: { color: this.randomColor(), isLocked: false },
+    };
+  }
+  //Returns a randon color hex code
+  randomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  randomize() {
+    if (!this.state.swatch1.isLocked)
+      this.setState({
+        swatch1: {
+          color: this.randomColor(),
+          isLocked: this.state.swatch1.isLocked,
+        },
+      });
+    if (!this.state.swatch2.isLocked)
+      this.setState({
+        swatch2: {
+          color: this.randomColor(),
+          isLocked: this.state.swatch2.isLocked,
+        },
+      });
+    if (!this.state.swatch3.isLocked)
+      this.setState({
+        swatch3: {
+          color: this.randomColor(),
+          isLocked: this.state.swatch3.isLocked,
+        },
+      });
+    if (!this.state.swatch4.isLocked)
+      this.setState({
+        swatch4: {
+          color: this.randomColor(),
+          isLocked: this.state.swatch4.isLocked,
+        },
+      });
+    if (!this.state.swatch5.isLocked)
+      this.setState({
+        swatch5: {
+          color: this.randomColor(),
+          isLocked: this.state.swatch5.isLocked,
+        },
+      });
+  }
+
+  toggleLock() {
+    this.setState({
+      swatch1: {
+        color: this.randomColor(),
+        isLocked: !this.state.swatch1.isLocked,
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='text-center bg-dark fixed-top'>
+          <button
+            className='btn btn-secondary m-1'
+            onClick={() => {
+              this.randomize();
+            }}>
+            RANDOMIZE COLORS
+          </button>
+        </div>
+        <div className='w-100 d-flex' style={{ height: '100vh' }}>
+          <Swatch
+            color={this.state.swatch1.color}
+            isLocked={this.state.swatch1.isLocked}
+            toggle={() => {
+              this.toggleLock();
+            }}
+          />
+          <Swatch
+            color={this.state.swatch2.color}
+            isLocked={this.state.swatch2.isLocked}
+            toggle={() => {
+              this.toggleLock();
+            }}
+          />
+          <Swatch
+            color={this.state.swatch3.color}
+            toggle={() => {
+              this.toggleLock();
+            }}
+          />
+          <Swatch
+            color={this.state.swatch4.color}
+            isLocked={this.state.swatch4.isLocked}
+            toggle={() => {
+              this.toggleLock();
+            }}
+          />
+          <Swatch
+            color={this.state.swatch5.color}
+            isLocked={this.state.swatch5.isLocked}
+            toggle={() => {
+              this.toggleLock();
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
